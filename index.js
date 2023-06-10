82d3d29c82483cb50713ed925aac863fa51a521f
dimsssss <caruen@hanmail.net>
2023-06-10 15:29:41 +0900

    init: first setting

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "env": {
+		"node": true,
+		"broswer": false
+  },
+	"parserOptions": {
+		"ecmaVersion": "es2016",
+		"sourceType": "module"
+	},
+	"rules": {
+		"eqeqeq": "warning"
+		"semi": ["error", "always"]
+	}
+}
+
